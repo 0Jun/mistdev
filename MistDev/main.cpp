224567017d97cbfbc6f82e32a404198817141b6c
Pusty <pusty8888@gmail.com>
2016-06-12 22:33:45 +0200

    square > cycle just testing stuff

--- MistDev/main.cpp
+++ MistDev/main.cpp
@@ -8,8 +8,9 @@ int main()
 {
 	//I MADE A CHANGE :O IT RUNS :O
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RectangleShape shape(sf::Vector2f(100,100));
+	shape.setPosition(sf::Vector2f(50, 50));
+	shape.setFillColor(sf::Color::Red);
 
 	while (window.isOpen())
 	{
