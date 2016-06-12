aab99b5e432075ec2334175511f4b89f1352dce8
Pusty <pusty8888@gmail.com>
2016-06-12 22:22:48 +0200

    test push, I hope I didn't mess anything up

--- MistDev/main.cpp
+++ MistDev/main.cpp
@@ -6,6 +6,7 @@
 
 int main()
 {
+	//I MADE A CHANGE :O IT RUNS :O
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
