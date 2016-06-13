a360fd2600d22abf12c5f9f552bd2a033c5dfc56
Pusty <pusty8888@gmail.com>
2016-06-14 00:49:02 +0200

    weird text input thingy

--- MistDev/main.cpp
+++ MistDev/main.cpp
@@ -12,6 +12,17 @@ int main()
 	shape.setPosition(sf::Vector2f(50, 50));
 	shape.setFillColor(sf::Color::Red);
 
+	std::string str;
+	sf::Text txt;
+	txt.setPosition(sf::Vector2f(0, 100));
+	txt.setColor(sf::Color::White);
+	sf::Font font;
+	if (!font.loadFromFile("arial.ttf"))
+		std::cout << "Failed to load font" << std::endl;
+	txt.setFont(font);
+	txt.setCharacterSize(24); 
+
+
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -19,10 +30,30 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::BackSpace)
+					str = str.substr(0, (str.size()) - 1);
+				if (event.key.code == sf::Keyboard::Return)
+					str = "";
+				txt.setString(str);
+			}
+			if (event.type == sf::Event::TextEntered)
+			{
+				if (event.text.unicode < 128)
+				{
+					str += static_cast<char>(event.text.unicode);
+					txt.setString(str);
+				}
+
+			}
+
+
 		}
 
 		window.clear();
 		window.draw(shape);
+		window.draw(txt);
 		window.display();
 	}
 
